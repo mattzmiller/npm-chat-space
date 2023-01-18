69715800711fa1aa8e810164f945f79f58742cf6
mzecic <matejzecic@gmail.com>
2023-01-18 09:35:33 -0500

    initial commit for creating an npm package for chatspace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ChatSpace from "./lib/components/ChatSpace.client";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ChatSpace />
     </div>
   );
 }
